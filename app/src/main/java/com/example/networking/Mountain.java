e5daeb90badc08ca53ffdc85e5f27d4844e1bde2
a21henhe <a21henhe@student.his.se>
2022-04-23 14:18:04 +0200

    created class mountain

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,27 @@
+package com.example.networking;
+
+public class Mountain {
+
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(){
+        name="Saknar namn";
+        location="Saknar location";
+        height=0;
+    }
+
+    public Mountain(String n, String l, int h){
+        name = n;
+        location=l;
+        height=h;
+    }
+
+    public String info(){
+        String tmp= new String();
+        tmp+=name+" is located in mountain range " + location + " and reaches " + height + "m above the sea.";
+        return tmp;
+    }
+
+}
